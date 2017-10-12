04244f258f90d870be80868c834ab5a388013ef5
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2017-10-12 14:25:37 +0200

    Fix date usage for alpine

--- rootfs/scripts/logging.lib.sh
+++ rootfs/scripts/logging.lib.sh
@@ -3,22 +3,22 @@
 # """
 _log(){
   declare BLUE="\e[32m" WHITE="\e[39m" BOLD="\e[1m" NORMAL="\e[0m"
-  echo -e "$(date --iso-8601=s)${BLUE}${BOLD} (info)${WHITE}:" $@${NORMAL}
+  echo -e "$(date -Is)${BLUE}${BOLD} (info)${WHITE}:" $@${NORMAL}
 }
 
 _error(){
   declare RED="\e[91m" WHITE="\e[39m"
-  echo -e "$(date --iso-8601=s)${RED} (error)${WHITE}:" $@
+  echo -e "$(date -Is)${RED} (error)${WHITE}:" $@
   exit 1
 }
 
 _debug()
 {
   declare BLUE="\e[36m" WHITE="\e[39m"
-  echo -e "$(date --iso-8601=s)${BLUE} (debug)${WHITE}:" $@
+  echo -e "$(date -Is)${BLUE} (debug)${WHITE}:" $@
 }
 
 _warning(){
   declare RED="\e[91m" WHITE="\e[39m"
-  echo -e "$(date --iso-8601=s)${RED} (warning)${WHITE}:" $@
+  echo -e "$(date -Is)${RED} (warning)${WHITE}:" $@
 }
