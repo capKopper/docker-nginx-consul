3e86ecfdf9bd5a5ff229ffdff023fcf68a0b5556
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2017-10-12 16:37:10 +0200

    Better log levels

--- rootfs/scripts/logging.lib.sh
+++ rootfs/scripts/logging.lib.sh
@@ -3,22 +3,22 @@
 # """
 _log(){
   declare BLUE="\e[32m" WHITE="\e[39m" BOLD="\e[1m" NORMAL="\e[0m"
-  echo -e "$(date -Is)${BLUE}${BOLD} (info)${WHITE}:" $@${NORMAL}
+  echo -e "$(date -Is)${BLUE}${BOLD} INFO${WHITE}" $@${NORMAL}
 }
 
 _error(){
   declare RED="\e[91m" WHITE="\e[39m"
-  echo -e "$(date -Is)${RED} (error)${WHITE}:" $@
+  echo -e "$(date -Is)${RED} ERRO${WHITE}" $@
   exit 1
 }
 
 _debug()
 {
   declare BLUE="\e[36m" WHITE="\e[39m"
-  echo -e "$(date -Is)${BLUE} (debug)${WHITE}:" $@
+  echo -e "$(date -Is)${BLUE} DEBU${WHITE}" $@
 }
 
 _warning(){
   declare RED="\e[91m" WHITE="\e[39m"
-  echo -e "$(date -Is)${RED} (warning)${WHITE}:" $@
+  echo -e "$(date -Is)${RED} WARN${WHITE}" $@
 }
