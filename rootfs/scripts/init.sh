bd064dcc0e610876028652a33d6a445d092c8e49
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2017-10-12 16:37:21 +0200

    Better comment

--- rootfs/scripts/init.sh
+++ rootfs/scripts/init.sh
@@ -117,7 +117,7 @@ function launch_consul_template {
     touch /consul-template/templates/nginx.conf.ctmpl
 
     if [ -n "${NGINX_DEBUG}" ]; then
-        _log "Starting consul-template -once..."
+        _log "Starting consul-template in ONCE mode..."
         consul-template -log-level ${CONSUL_LOGLEVEL} \
                         -template /consul-template/templates/nginx.conf.ctmpl.in:/consul-template/templates/nginx.conf.ctmpl \
                         ${ctargs} -once
